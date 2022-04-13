8c6aecd46e8deb533a4bf63eccb55c538c83fc5d
Nathan Galley <45778854+Nbobito@users.noreply.github.com>
2022-04-12 23:36:55 -0600

    Implement greedy algorithm
    It sucks, but it got the assignment done.

--- compress.py
+++ compress.py
@@ -1,36 +1,37 @@
 import re
 
-def compress(str):
-    stripped = re.sub('\s{2,}', ' ', re.sub('\n', ' ', str))
-    array = stripped.split(' ')
-    
-    out = ""
-    for i in array:
-        if i == "ffffff":
-            out += "00"
-        elif i == "000000":
-            out += "01"
-        elif i == "ff0000":
-            out += "10"
-        elif i == "0000ff":
-            out += "11"
+def compress(string):
+    string = string.lower()
+    clusters = []
+    for i in range(0, len(string)):
+        for k in range(i + 2, len(string) + 1):
+            section = string[i:k]
+            clusters.append((string.count(section) * (k - i) - k + i, section))
+
+
+    clusters = sorted(clusters)
+    clusters.reverse()
+    out = []
+    for i in clusters:
+        if (i not in out) and (i[0] > 2):
+            out.append(i)
+
     return out
 
-def decompress(str):
-    array = re.findall('.{1,2}', str)
-    out = ""
-    for i in array:
-        if i == "00":
-            out += "ffffff"
-        elif i == "01":
-            out += "000000"
-        elif i == "10":
-            out += "ff0000"
-        elif i == "11":
-            out += "0000ff"
-    return out
-    
-compressed = compress(image)
-decompressed = decompress(compressed)
 
-print(decompressed)
+def get_dict(string, depth):
+    alphabet = "☀☂☃☄★☆☇☈☉☊☋☌☍'☎☏☐☑☒☓☖"
+    greedy_string = string.lower()
+    out = []
+    for i in range(0, depth):
+        next = compress(greedy_string)[0]
+        out.append(next)
+        replacent = next[1]
+        greedy_string = greedy_string.replace(replacent, alphabet[i])
+    return out
+
+
+lyrics = '''
+The_Man_by_Aloe_Blacc_Well_you_can_tell_everybody_Yeah_you_can_tell_everybody_Go_ahead_and_tell_everybody_I'm_the_man,_I'm_the_man,_I'm_the_man_Well_you_can_tell_everybody_Yeah_you_can_tell_everybody_Go_ahead_and_tell_everybody_I'm_the_man,_I'm_the_man,_I'm_the_man_Yes_I_am,_yes_I_am,_yes_I_am_I'm_the_man,_I'm_the_man,_I'm_the_man
+'''
+print(get_dict(lyrics, 3))
